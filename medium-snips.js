b07cfad16e2aa5c3b3d71f80147ccd3241ee6598
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-23 19:10:18 -0400

    more promises

--- medium-snips.js
+++ medium-snips.js
@@ -1,28 +1,111 @@
-function washLaundry() {
-    return new Promise ((resolve,reject) => {
-        resolve('The laundry is clean!');
-    }else {
-        reject(Error('Error'));
-    }
-)}
+
+// const newPromise = new Promise((resolve, reject) => {
+//   const res = true;
+//   if (res) {
+//     resolve("Success!");
+//   } else {
+//     reject(Error("Error!"));
+//   }
+// });
 
 
+// const washLaundry = new Promise((resolve, reject) => {
+//   const res = true;
+//   setTimeout(() => {
+//     if (res) {
+//       resolve("The laundry is clean!");
+//     } else {
+//       reject(Error("Error"));
+//     }
+//   }, 2000);
+// });
 
-const washLaundry = new Promise((resolve, reject)=> {
-    const res = true;
-    if (res) {
-        resolve('The laundry is clean!');
-    }else {
-        reject(Error('Error'));
-    }
+// washLaundry.then(
+//   (res) => console.log(res),
+//   (err) => console.log(err)
+// );
+
+
+// function washLaundry() {
+//   return new Promise((resolve, reject) => {
+//     if (res) {
+//       resolve("The laundry is clean!");
+//     } else {
+//       reject(Error("Error"));
+//     }
+//   });
+// }
+
+
+// const washLaundry = new Promise((resolve, reject) => {
+//   const res = true;
+//   if (res) {
+//     resolve("The laundry is clean!");
+//   } else {
+//     reject(Error("Error"));
+//   }
+// });
+
+// washLaundry.then(
+//   (res) => console.log(res),
+//   (err) => console.log(err)
+// );
+
+
+// const washLaundry = new Promise((reject, resolve) => {
+//   const res = true;
+//   if (res) {
+//     resolve("The laundry is clean!");
+//   } else {
+//     reject(Error("Error"));
+//   }
+// });
+
+// washLaundry.then(
+//   (res) => console.log(res),
+//   (err) => console.log(err)
+// );
+
+// const washLaundry = new Promise((resolve, reject) => {
+//   resolve("The laundry is clean!");
+//   reject("Error");
+// });
+
+// washLaundry.then((res) => {
+//   console.log(res);
+// });
+// washLaundry.catch((err) => {
+//   alert(err);
+// });
+
+
+// const washLaundry = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("The laundry is clean!");
+//     reject("Error");
+//   }, 2000);
+// });
+
+// washLaundry.then((res) => {
+//   console.log(res);
+// });
+// washLaundry.catch((err) => {
+//   alert(err);
+// });
+
+
+const washLaundry = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("The laundry is clean!");
+    reject("Error");
+  }, 2000);
 });
+console.log("I'm going to do laundry!");
 
-washLaundry.then((res) => console.log(res), (err) => alert(err));
-
-
-
-const dryLaundry = new Promise((resolve, reject) => {
-    resolve('The laundry is dry!');
+washLaundry.then((res) => {
+  console.log(res);
 });
-
-dryLaundry.then().catch(err)
+washLaundry.catch((err) => {
+  alert(err);
+});
+console.log("The laundry isn't ready, I'm going to go take a nap.");
