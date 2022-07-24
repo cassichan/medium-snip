9013ece293da419af259cecb0ebe63d847b4b188
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-24 16:11:37 -0400

    scope

---
+++ scope.js
@@ -0,0 +1,20 @@
+// function addTwoNum (num1, num2) {
+//     let sum = num1 + num2;
+//     console.log(sum)
+// }
+
+// addTwoNum(2,2)
+
+let sum = 9;
+
+function addTwoNum (num1, num2) {
+    return sum = num1 + num2;
+}
+
+console.log(sum);
+(addTwoNum(2,3));
+console.log(sum)
+
+
+
+
